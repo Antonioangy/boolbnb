63304383eba21ff98ac31b913ced63e1ba114c3d
ParisiA02 <andreaparisi4502@gmail.com>
2022-02-24 16:08:51 +0100

    push test

--- database/migrations/2022_02_24_145845_create_apartments_table.php
+++ database/migrations/2022_02_24_145845_create_apartments_table.php
@@ -15,6 +15,7 @@ class CreateApartmentsTable extends Migration
     {
         Schema::create('apartments', function (Blueprint $table) {
             $table->id();
+            //commento di test
             $table->timestamps();
         });
     }
