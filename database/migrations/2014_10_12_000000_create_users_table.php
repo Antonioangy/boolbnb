d62f9fc68ef64b8bbcebc323c532dc3a6d4e6c1c
BarbonFisso <barbieri.mat@hotmail.it>
2022-02-28 15:24:53 +0100

    Fix: register form

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('last_name');
-            $table->date('birth_day');
+            $table->string('last_name')->nullable();
+            $table->date('birth_day')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
