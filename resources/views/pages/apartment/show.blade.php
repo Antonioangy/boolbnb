c583f1ce45576225c2d784af65513d908421c9e3
FLU <valerio.massj@gmail.com>
2022-03-02 18:05:13 +0100

    update: delete access.blade delete routes

--- resources/views/pages/apartment/show.blade.php
+++ resources/views/pages/apartment/show.blade.php
@@ -19,8 +19,8 @@
         </div>
         {{-- SHOW APPARTAMENTO --}}
         <div class="show_apartment w-100 h-100">
-            @if ($apartment->images)  
-                <img class="w-100" src="{{ asset('storage/assets/appartamento.jpg') }}" alt="">
+            @if ($apartment->images)
+                <img class="w-100" src="{{asset('storage/assets/'. $apartment->images)}}" alt="{{$apartment -> title}}">
             @endif
             <p>
                 {{ $apartment -> description }}
