0509d69035e401566ab581e60d0e31fe6b00c868
FLU <valerio.massj@gmail.com>
2022-03-02 16:27:16 +0100

    add: red color

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,22 +1,25 @@
 <header>
     <div>
+        {{-- navbar header --}}
         <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
+            {{-- logo --}}
             <a class="navbar-brand" href="{{ route('home') }}">
                 <img src="{{asset('storage/images/boolbnb-logo.png')}}" alt="bnb logo">
             </a>
 
             <div>
+                {{--  --}}
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item active">
-                            <a class="nav-link orange" href="#">Home <span class="sr-only">(current)</span></a>
+                            <a class="nav-link orange" href="{{ route('home') }}">Home</a>
                         </li>
                         @auth
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('user.dashboard') }}"><i class="fa-solid fa-user orange"></i></a>
+                        <li class="nav-item rounded-circle border border-orange p-5">
+                            <a class="nav-link" href="{{ route('user.dashboard') }}"><i class="fa-solid fa-user text-orange w-100"></i></a>
                         </li>
                         @else
                         <li class="nav-item ">
