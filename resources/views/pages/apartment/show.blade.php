13b83a1847ca684b5981db35e1ff9a615897e019
Antoniok2 <antoniopiazza18@gmail.com>
2022-03-02 13:05:09 +0100

    add style show apartment

--- resources/views/pages/apartment/show.blade.php
+++ resources/views/pages/apartment/show.blade.php
@@ -7,36 +7,58 @@
 
 @section('content')
 
-    <h2>
-        {{ $apartment -> title }}
-    </h2>
+    <div class="container">
+        {{-- TITOLO APPARTAMENTO E LOCALITA --}}
+        <div class="general_info">
+            <h3>
+                {{ $apartment -> title }}
+            </h3>
+            <h5>
+                {{ $apartment -> address }}
+            </h5>
+        </div>
+        {{-- SHOW APPARTAMENTO --}}
+        <div class="show_apartment w-100 h-100">
+            {{-- @if ($apartment->images)   --}}
+                {{-- <img src="{{asset('storage/assets/'. $apartment->images)}}" alt="{{$apartment -> title}}"> --}}
+                <img class="w-100" src="{{ asset('storage/assets/appartamento.jpg') }}" alt="">
+            {{-- @endif --}}
+            <p>
+                {{ $apartment -> description }}
+            </p>
+        </div>
+        {{-- SERVIZI DISPONIBILI APPARTAMENTO --}}
+        <div class="services">
+            <h3>Cosa troverai</h3>
+        </div>
 
-    <p>
-        {{ $apartment -> description }}
-    </p>
 
-    <span class="mx-2">
-        Stanze: {{ $apartment -> rooms }}
-    </span>
-    <span class="mx-2">
-        Letti: {{ $apartment -> beds }}
-    </span>
-    <span class="mx-2">
-        Bagni: {{ $apartment -> bathrooms }}
-    </span>
-    <span class="mx-2">
-        Metri quadri: {{ $apartment -> sq }}
-    </span>
+    
+        <span class="mx-2">
+            Stanze: {{ $apartment -> rooms }}
+        </span>
+        <span class="mx-2">
+            Letti: {{ $apartment -> beds }}
+        </span>
+        <span class="mx-2">
+            Bagni: {{ $apartment -> bathrooms }}
+        </span>
+        <span class="mx-2">
+            Metri quadri: {{ $apartment -> sq }}
+        </span>
+    
 
-    @if ($apartment->images)  
-    <img src="{{asset('storage/assets/'. $apartment->images)}}" alt="{{$apartment -> title}}">
-    @endif
+    
 
-    <div>
-        {{ $apartment -> address }}
-    </div>
+    
 
-    <div>
+    
+        @if ($apartment -> longitude != '')
+        <apartment-map :lng="{{ $apartment -> longitude }}" :lat="{{ $apartment -> latitude }}"></apartment-map>
+        @endif
+        
+        <a href="{{ route('home') }}">BACK</a>
+
         <h3>Invia una mail al proprietario</h3>
         <form action="{{ route('apartment.message_send', $apartment -> id) }}" method="POST">
             @method('POST')
@@ -58,13 +80,9 @@
 
             <input type="submit" value="Invia" class="btn btn-success">
         </form>
-
     </div>
 
-    @if ($apartment -> longitude != '')
-    <apartment-map :lng="{{ $apartment -> longitude }}" :lat="{{ $apartment -> latitude }}"></apartment-map>
-    @endif
-    
-    <a href="{{ route('home') }}">BACK</a>
+
+   
     
 @endsection
