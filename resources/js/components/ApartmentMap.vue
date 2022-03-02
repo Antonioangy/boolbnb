d3c5f64398d00fd033841ede626c7fa1b07df0da
BarbonFisso <barbieri.mat@hotmail.it>
2022-03-02 11:06:13 +0100

    Add: map in apartment show

---
+++ resources/js/components/ApartmentMap.vue
@@ -0,0 +1,70 @@
+<template>
+    <div id="map"></div>
+</template>
+
+<script>
+// fonte https://developer.tomtom.com/blog/build-different/using-tomtom-maps-vue-3
+
+export default {
+    name: 'Map',
+    props: {
+        lng: Number,
+        lat: Number,
+    },
+
+    data() {
+        return {
+            apiKey: 'CCNCApHEo3ZS5ewtvHkSDu7hMeAY7sQc', //tua key personale da developer.tomtom
+            tt: window.tt,
+            coordinates: [this.lng,this.lat],
+        }
+    },
+
+    methods: {
+        getMap() {
+            let map = this.tt.map({
+                key: this.apiKey,
+                container: 'map',
+                center: this.coordinates,
+                zoom: 14
+            });
+            map.addControl(new this.tt.FullscreenControl()); 
+            map.addControl(new this.tt.NavigationControl());
+            this.addMarker(map);
+        },
+
+        addMarker(map) { 
+            let popupOffset = 25; 
+        
+            var marker = new this.tt.Marker().setLngLat(this.coordinates).addTo(map); 
+            var popup = new this.tt.Popup({ offset: popupOffset }); 
+            this.reverseGeocoding(marker, popup); 
+            marker.setPopup(popup).togglePopup(); 
+        },
+
+        reverseGeocoding(marker, popup) { 
+            this.tt.services.reverseGeocode({ 
+            key: 'iTF86GRA2V5iGjM6LMMV54lrK8v6zC1w', 
+            position: marker.getLngLat() 
+            })
+            .then( function( result ){ 
+            console.log(result.addresses[0].address.freeformAddress); 
+            popup.setHTML(result.addresses[0].address.freeformAddress); 
+            }) 
+        }
+    },
+
+    mounted() {
+        console.log('map mounted');
+        this.getMap();
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+#map {
+  height: 90vh;
+  width: 90vw;
+}
+</style>
--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title')</title>
 
+    {{-- TomTom --}}
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.18.0/maps/maps-web.min.js"></script>
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.18.0/services/services-web.min.js"></script>
+    <link rel='stylesheet' type='text/css' href='https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.18.0/maps/maps.css'>
+    
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     <script src="{{ asset('/js/app.js') }}"></script>
-
+    
+    <title>@yield('title')</title>
 </head>
 <body>
 
