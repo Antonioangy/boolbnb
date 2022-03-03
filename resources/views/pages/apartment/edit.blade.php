e6b569d0acf644f99fbdec84f9f081d96bf3201f
Antoniok2 <antoniopiazza18@gmail.com>
2022-03-03 16:40:59 +0100

    fix: add label on edit apartment

--- resources/views/pages/apartment/edit.blade.php
+++ resources/views/pages/apartment/edit.blade.php
@@ -8,23 +8,31 @@
         @method('POST')
         @csrf
 
-        <div class="insert-container container-xl bg-darkBlue d-flex flex-column p-5 rounded">
+        <div class="insert-container container-xl bg-darkBlue d-flex flex-column p-5 rounded text-white">
             <h1 class="text-white">Aggiorna appartamento</h1>
 
+            <label for="title">Titolo</label>
             <input type="text" name="title" placeholder="Titolo" class="p-2 h5" value="{{$apartment -> title}}"> <br>
             
+            <label for="description">Descrizione</label>
             <input type="text" name="description" placeholder="Descrizione" class="p-2 h5" value="{{$apartment -> description}}"><br>
             
+            <label for="rooms">Stanze</label>
             <input type="number" name="rooms" placeholder="Stanze" min="0" class="p-2 h5" value="{{$apartment -> rooms}}"><br>
     
+            <label for="beds">Letti</label>
             <input type="number" name="beds" placeholder="Letti" min="0" class="p-2 h5" value="{{$apartment -> beds}}"><br>
             
+            <label for="bathrooms">Bagni</label>
             <input type="number" name="bathrooms" placeholder="Bagni" min="0" class="p-2 h5" value="{{$apartment -> bathrooms}}"><br>
             
+            <label for="sq">Dimensione appartamento</label>
             <input type="number" name="sq" placeholder="Metri Quadri" min="0" class="p-2 h5" value="{{$apartment -> sq}}"><br>
     
+            <label for="images">Immagine appartamento</label>
             <input type="file" name="images" class="text-white"><br>
     
+            <label for="address">Indirizzo appartamento</label>
             <input type="text" name="address" placeholder="Address" class="p-2 h5" value="{{$apartment -> address}}"><br>
         </div>
 
