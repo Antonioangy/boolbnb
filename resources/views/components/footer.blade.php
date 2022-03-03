34a17b32d56e8fbbd642b1520849eed584e16de2
Antonioangy <dumitruangy26@gmail.com>
2022-03-03 17:46:04 +0100

    footer aggiornato

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,86 +1,46 @@
-<footer>
-    <div class="container-fluid mt-5  border-top border-secondary bg-darkBlue text-white">
-        <div class="row ">
-            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 mt-4">
-                <h3>Assistenza</h3>
-                <ul class="list-unstyled">
-                    <li>
-                        Centro Assistenza
-                    </li>
-                    <li>
-                        Informazioni di sicurezza
-                    </li>
-                    <li>
-                        Opzioni di cancellazione
-                    </li>
-                    <li>
-                        La nostra risposta all'emergenza COVID-19
-                    </li>
-                    <li>
-                        Accessibilità per tutti
-                    </li>
-                    <li>
-                        Segnala problemi
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 mt-4">
-                <h3>Community</h3>
-                <ul class="list-unstyled">
-                    <li>
-                        Boolbnb.org: un rifugio sicuro
-                    </li>
-                    <li>
-                        Supporta i rifugiati afghani
-                    </li>
-                    <li>
-                        Contro la discriminazione
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 mt-4">
-                <h3>Ospitare</h3>
-                <ul class="list-unstyled">
-                    <li>
-                        Prova a ospitare
-                    </li>
-                    <li>
-                        AirCover: host protetti
-                    </li>
-                    <li>
-                        Esplora le risorse per host
-                    </li>
-                    <li>
-                        Vai al forum della community
-                    </li>
-                    <li>
-                        Come ospitare responsabilmente
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 mt-4">
-                <h3>Informazioni</h3>
-                <ul class="list-unstyled">
-                    <li>
-                        Newsroom
-                    </li>
-                    <li>
-                        Scopri le nuove funzionalità
-                    </li>
-                    <li>
-                        Lettera dai nostri fondatori
-                    </li>
-                    <li>
-                        Opportunità di lavoro
-                    </li>
-                    <li>
-                        Investitori
-                    </li>
-                    <li>
-                        Airbnb Luxe
-                    </li>
-                </ul>
+<footer class="border-secondary bg-darkBlue">
+    <div class="container text-white">
+        <div class="row pt-4">
+            <div class="col-md-12 col-lg-12">
+                <div class="row">
+                    <div class="col-md-3 mb-md-0 mb-3 ">
+                        <h2 class="footer-heading">Assistenza</h2>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-headset"></i></span>Centro Assistenza</a></li>
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-shield"></i></span>Informazioni di sicurezza</a></li>
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-times"></i></span>Opzioni di cancellazione</a></li>
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-briefcase-medical"></i></span>Soluzioni all'emergenza COVID-19</a></li>
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-blind"></i></span>Accessibilità per tutti</a></li>
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-exclamation-circle"></i></span>Segnala problemi</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 mb-md-0 mb-3">
+                        <h2 class="footer-heading">Community</h2>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-house-user"></i></span>Boolbnb.org: un rifugio sicuro</a></li>
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-house-user"></i></span>Supporta i rifugiati afghani</a></li>
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-user-times"></i></span>Contro la discriminazione</a></li>
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fab fa-internet-explorer"></i></span>Vai al forum della community</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 mb-md-0 mb-3">
+                        <h2 class="footer-heading">Ospitare</h2>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-laptop-house"></i></span>Prova a ospitare</a></li>
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-home"></i></span>BoolCover: host protetti</a></li>
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-home"></i></span>Come ospitare responsabilmente</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 mb-md-0 mb-3">
+                        <h2 class="footer-heading">Informazioni</h2>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-plus"></i></span>Scopri le nuove funzionalità</a></li>
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-crown"></i></span>Boolbnb Luxury</a></li>
+                            <li><a href="#" class="py-1 d-block text-white"><span class="mr-2"><i class="fas fa-envelope"></i></span>Lettera dai nostri fondatori</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
-      </div>
+    </div>
 </footer>
