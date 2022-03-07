bf90ce78fb8895393a6a673e8d9be193d2355625
BarbonFisso <barbieri.mat@hotmail.it>
2022-03-08 00:39:43 +0100

    Fix: map center at search

--- resources/js/components/SearchInMap.vue
+++ resources/js/components/SearchInMap.vue
@@ -8,32 +8,16 @@
 export default {
 
     name: 'SearchInMap',
+    props: {
+        // prendo le cordinate dell ricerca per spostare la mappas
+        center: Array
+    },
+    
     data() {
         return {
             apiKey: 'CCNCApHEo3ZS5ewtvHkSDu7hMeAY7sQc', //tua key personale da developer.tomtom
             tt: window.tt,
             coordinates: [12.395915,41.9102415],
-            geometryList: [
-                // {
-                //     type: 'POLYGON',
-                //     vertices: [
-                //     '37.7524152343544,-122.43576049804686',
-                //     '37.70660472542312,-122.43301391601562',
-                //     '37.712059855877314,-122.36434936523438',
-                //     '37.75350561243041,-122.37396240234374'
-                //     ]
-                // },
-                // {
-                //     type: 'CIRCLE',
-                //     position: '37.71205,-121.36434',
-                //     radius: 6000
-                // },
-                {
-                    type: 'CIRCLE',
-                    position: '12.395915,41.9102415',
-                    radius: 20000
-                }
-            ]
         }
     },
 
@@ -43,38 +27,38 @@ export default {
             let map = this.tt.map({
                 key: this.apiKey,
                 container: 'map',
-                center: this.coordinates,
-                zoom: 14,
-                language: 'IT'
+                center: this.center,
+                zoom: 13,
+                language: 'it-IT'
             });
             map.addControl(new this.tt.FullscreenControl()); 
             map.addControl(new this.tt.NavigationControl());
-            this.addMarker(map, this.coordinates);
-            this.search(map);
+            // this.addMarker(map, this.center);
+            // this.search(map);
         },
 
-        search(map) {
+        // search(map) {
 
-            // tt.services.geometrySearch({s
-            //     key: this.apiKey,
-            //     geometryList: this.geometryList,
-            //     query: 'pizzeria',
-            //     // idxSet: 'PAD,Addr'
-            // }).then(r => console.log(r));
+        //     // tt.services.geometrySearch({s
+        //     //     key: this.apiKey,
+        //     //     geometryList: this.geometryList,
+        //     //     query: 'pizzeria',
+        //     //     // idxSet: 'PAD,Addr'
+        //     // }).then(r => console.log(r));
 
-            tt.services.nearbySearch({
-                key: this.apiKey,
-                center: this.coordinates,
-                radius: 20000,
-                limit: 30,
-                }).then(r => {
-                    r.results.forEach(element => {
-                        // console.log(element.position)
-                        let position = element.position;
-                        this.addMarker(map, position);
-                    });
-                });
-        },
+        //     tt.services.nearbySearch({
+        //         key: this.apiKey,
+        //         center: this.coordinates,
+        //         radius: 20000,
+        //         limit: 30,
+        //         }).then(r => {
+        //             r.results.forEach(element => {
+        //                 // console.log(element.position)
+        //                 let position = element.position;
+        //                 this.addMarker(map, position);
+        //             });
+        //         });
+        // },
 
         addMarker(map, coordinates) { 
             let popupOffset = 25; 
@@ -91,7 +75,6 @@ export default {
             position: marker.getLngLat() 
             })
             .then( function( result ){ 
-            // console.log(result.addresses[0].address.freeformAddress); 
             popup.setHTML(result.addresses[0].address.freeformAddress); 
             }) 
         }
@@ -108,7 +91,7 @@ export default {
 
 <style>
 #map {
-  height: 50vh;
-  width: 50vw;
+  height: 100%;
+  width: 100%;
 }
 </style>
