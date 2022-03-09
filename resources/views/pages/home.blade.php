37ea59283fe1bcc48ffa908d488a04c62bd50d42
Antonioangy <dumitruangy26@gmail.com>
2022-03-09 18:46:24 +0100

    cambiamenti ricerca in home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,12 +8,13 @@
 
 {{-- jumbotron --}}
 <section id="jumbotron" class="mb-5 d-flex">
-    
     <div class="search_container m-auto">
         <form action="{{ route('apartments.results') }}" method="POST">
             @csrf
-            <input type="text" name="addressQuery" placeholder="Dove vuoi andare?">
-            <input type="submit" value="Cerca">
+                <div class="input-group">
+                    <input type="search" class="form-control rounded" placeholder="dove vuoi andare?" aria-label="Search" aria-describedby="search-addon" />
+                    <button type="button" class="btn bg-light"><i class="fa fa-search"></i></button>
+                </div>
         </form>
     </div>
 
