e3397c0b0af7c9e2bc5e567432f3badd2603da1a
Antoniok2 <antoniopiazza18@gmail.com>
2022-03-11 11:14:44 +0100

    fix: responsive sponsor page

--- public/js/app.js
+++ public/js/app.js
@@ -52209,8 +52209,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Boolean Esercizi\boolbnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Boolean Esercizi\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Antonino\Desktop\boolbnb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Antonino\Desktop\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/pages/apartment/sponsor.blade.php
+++ resources/views/pages/apartment/sponsor.blade.php
@@ -4,10 +4,10 @@
 @endsection
 
 @section('content')
-    <div class="container py-5">
+    <div class="container">
         <h1 class="text-red">{{$apartment -> title}}</h1>
-        <div class="row pay_sponsor">
-            <div class="col-xl-4 left">
+        <div class="row pay_sponsor shadow p-3 my-3 bg-white rounded">
+            <div class="col-xl-4 col-lg-4 col-md-12 left">
                 <div class="photo">
                     <img src="{{asset('storage/assets/'. $apartment->images)}}" alt="">
                 </div>
@@ -22,7 +22,7 @@
                     </div>
                 @endif
             </div>
-            <div class="col-xl-8">
+            <div class="col-xl-8 col-lg-4 col-md-12 right">
                 <h1>Dai maggiore visibilità al tuo appartamento</h1>
                 <h3 class="text-red">Basic</h3>
                 <span>Metti in evidenza il tuo appartamento per <b>1</b> giorno.</span>
@@ -37,63 +37,59 @@
                     id="payment-form">
                         @csrf
 
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group required'>
-                                <label class='control-label text-blue'>Livello</label>
-                                {{-- <input name="amount" class='form-control' size='4' type='number'> --}}
-                                <select name="amount" class='form-control'>
-                                    <option value="2.99">Standard - 2.99 &euro;</option>
-                                    <option value="5.99">Premium - 5.99 &euro;</option>
-                                    <option value="9.99">Elite - 9.99 &euro;</option>
-                                </select>
-                            </div>
+                    <div class='form-row'>
+                        <div class='form-group required'>
+                            <label class='control-label text-blue'>Livello</label>
+                            {{-- <input name="amount" class='form-control' size='4' type='number'> --}}
+                            <select name="amount" class='form-control'>
+                                <option value="2.99">Standard - 2.99 &euro;</option>
+                                <option value="5.99">Premium - 5.99 &euro;</option>
+                                <option value="9.99">Elite - 9.99 &euro;</option>
+                            </select>
                         </div>
+                    </div>
 
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group required'>
-                                <label class='control-label text-blue'>Nome e Cognome</label> <input
-                                    class='form-control' size='4' type='text' value="test">
-                            </div>
+                    <div class='form-row'>
+                        <div class='form-group required'>
+                            <label class='control-label text-blue'>Nome e Cognome</label> <input
+                                class='form-control' size='4' type='text' value="test">
                         </div>
+                    </div>
 
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group card required'>
-                                <label class='control-label text-blue'>Numero della Carta</label> <input
-                                    autocomplete='off' class='form-control card-number' size='20'
-                                    type='text' value="4242 4242 4242 4242">
-                            </div>
+                    <div class='form-row'>
+                        <div class='form-group card required'>
+                            <label class='control-label text-blue'>Numero della Carta</label>
+                            <input autocomplete='off' class='form-control card-number' size='20' type='text' value="4242 4242 4242 4242">
                         </div>
+                    </div>
 
-                        <div class='form-row row'>
-                            <div class='col-xs-12 col-md-4 form-group cvc required'>
-                                <label class='control-label text-blue'>CVV</label> <input autocomplete='off'
-                                    class='form-control card-cvc' placeholder='ex. 311' size='4'
-                                    type='text' value="123">
-                            </div>
-                            <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                <label class='control-label text-blue'>Mese di scadenza</label> <input
-                                    class='form-control card-expiry-month' placeholder='MM' size='2'
-                                    type='text' value="02">
-                            </div>
-                            <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                <label class='control-label text-blue'>Anno di scadenza</label> <input
-                                    class='form-control card-expiry-year' placeholder='YYYY' size='4'
-                                    type='text' value="2024">
-                            </div>
+                    <div class='form-row'>
+                        <div class='form-group cvc required'>
+                            <label class='control-label text-blue'>CVV</label>
+                            <input autocomplete='off' class='form-control card-cvc' placeholder='ex. 311' size='4' type='text' value="123">
                         </div>
+                        <div class='form-group expiration required'>
+                            <label class='control-label text-blue'>Mese di scadenza</label>
+                            <input class='form-control card-expiry-month' placeholder='MM' size='2' type='text' value="02">
+                        </div>
+                        <div class='form-group expiration required'>
+                            <label class='control-label text-blue'>Anno di scadenza</label>
+                            <input class='form-control card-expiry-year' placeholder='YYYY' size='4' type='text' value="2024">
+                        </div>
+                    </div>
 
-                        <div class='form-row row'>
-                            <div class='col-md-12 error form-group hide'>
-                                <div class='alert-danger alert'>Please correct the errors and try
-                                    again.</div>
+                    <div class='form-row'>
+                        <div class='error form-group hide'>
+                            <div class='alert-danger alert'>
+                                Please correct the errors and tryagain.
                             </div>
                         </div>
+                    </div>
 
-                        <div>
-                            <button class="btn btn-blue mb-2" type="submit">Paga ora</button>
-                        </div>
-                            
-                        </form>
+                    <div>
+                        <button class="btn btn-blue mb-2" type="submit">Paga ora</button>
+                    </div>            
+                </form>
             </div>
         </div>
     </div>
